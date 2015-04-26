45991bc245396fa555cdbfe0273fc8214eb80126
Tobias Gassmann <tobias@gaszmann.de>
2015-04-26 17:44:08 +0200

    update to new components

--- src/android-image-uploader.rb
+++ src/android-image-uploader.rb
@@ -15,12 +15,11 @@ puts 'Starting...'
 
 authorization_code_value = ARGV.shift
 
-client = OAuth2::Client.new('image_uploader','123456', :site => 'http://test.zeitfaden.com');
+client = OAuth2::Client.new('553cf9b43f5cb781589','SoPCz4GzYcUOUMfHpz1Bkw489uVdHDYA2OcBSSaR', :site => 'http://test.zeitfaden.com');
 
 client.auth_code.authorize_url(:redirect_uri => 'http://myretrunlocalhost:8080/oauth2/callback')
 
-
-token = client.auth_code.get_token(authorization_code_value, :redirect_uri => 'http://asdlocalhost:8080/oauth2/callback', :headers => {'Authorization' => 'Basic some_password'})
+token = client.auth_code.get_token(authorization_code_value, :redirect_uri => 'http://www.gaszmann.de/excel_oauth_app/', :headers => {'Authorization' => 'Basic some_password'})
 
 puts token.to_hash
 my_access_token = token.to_hash[:access_token]
@@ -31,82 +30,73 @@ puts my_access_token
 myImages = Dir["*.jpg"] 
 
 myImages.each { |image_file_name| 
-	puts image_file_name 
-	puts 'this is a ffile'
-	
-	a = EXIFR::JPEG.new(image_file_name)
-	
-    begin
-	
-		latitude = a.exif[0].gps_latitude[0].to_f + (a.exif[0].gps_latitude[1].to_f / 60) + (a.exif[0].gps_latitude[2].to_f / 3600)
-		longitude = a.exif[0].gps_longitude[0].to_f + (a.exif[0].gps_longitude[1].to_f / 60) + (a.exif[0].gps_longitude[2].to_f / 3600)
-		 
-		longitude = longitude * -1 if a.exif[0].gps_longitude_ref == "W"   # (W is -, E is +)
-		latitude = latitude * -1 if a.exif[0].gps_latitude_ref == "S"      # (N is +, S is -)
-
-    rescue NoMethodError
-		latitude = 0
-		longitude = 0
-	
-	end
-	
-	
-	puts latitude.to_s
-	puts longitude.to_s
-
-	myDate = EXIFR::JPEG.new(image_file_name).date_time
-
-	item_id = Digest::MD5.hexdigest(image_file_name + myDate.to_s + latitude.to_s + longitude.to_s)
-	puts item_id
-
+  begin
+    puts image_file_name 
+    puts 'this is a ffile'
+    
+    exif_data = EXIFR::JPEG.new(image_file_name)
+    
+    
+    puts exif_data.gps.latitude.to_s
+    puts exif_data.gps.longitude.to_s
+  
+    item_id = Digest::MD5.hexdigest(exif_data.date_time.to_s + exif_data.gps.latitude.to_s + exif_data.gps.longitude.to_s + File.size(image_file_name).to_s + exif_data.model + exif_data.exposure_time.to_s + exif_data.f_number.to_s)
+    puts item_id
+    puts File.size(image_file_name).to_s
+    puts exif_data.date_time.to_s
+    
+  
     application_data = {
-		:item_id => item_id,
-    	:camera_model => EXIFR::JPEG.new(image_file_name).model,
-		:exposure_time => EXIFR::JPEG.new(image_file_name).exposure_time.to_s,
-		:f_number => EXIFR::JPEG.new(image_file_name).f_number.to_f
+      :item_id => item_id,
+      :camera_model => exif_data.model,
+      :exposure_time => exif_data.exposure_time.to_s,
+      :f_number => exif_data.f_number.to_f
+    }
+  
+    
+  
+    RestClient.post("http://test.zeitfaden.com/station/upsert/?access_token=#{my_access_token}",
+      :uploadFile => File.new(image_file_name),
+      :startLatitude => exif_data.gps.latitude,
+      :startLongitude => exif_data.gps.longitude,
+      :endLatitude => exif_data.gps.latitude,
+      :endLongitude => exif_data.gps.longitude,
+  
+      :startTimestamp => exif_data.date_time.to_time.to_i,
+      :endTimestamp => exif_data.date_time.to_time.to_i,
+  
+      :publishStatus => 'public',
+      :appItemId => item_id,
+      :appData => JSON.generate(application_data)
+      
+    ){ |response, request, result, &block|
+      puts "######################################################################################### this is the respinse code #{response.code}"
+      case response.code
+      when 200
+        p "It worked !"
+        #puts response
+        #puts result
+        #puts response.headers
+        #response
+      when 423
+        put "something 423?"
+        exit
+        #raise SomeCustomExceptionIfYouWant
+      else
+        #puts response
+        puts "SOMETHING WENT WRONG, ABORTING."
+        exit
+        
+        #response.return!(request, result, &block)
+      end
     }
 
-
-	puts myDate.to_s
-	
-
-      RestClient.post("http://test.zeitfaden.com/station/upsert/?access_token=#{my_access_token}",
-        :uploadFile => File.new(image_file_name),
-        :startLatitude => latitude,
-        :startLongitude => longitude,
-        :endLatitude => latitude,
-        :endLongitude => longitude,
-		
-        :startDate => myDate.to_s,
-        :endDate => myDate.to_s,
-		
-        :publishStatus => 'public',
-        :applicationItemId => item_id,
-        :applicationData => JSON.generate(application_data)
-        
-      ){ |response, request, result, &block|
-        puts "######################################################################################### this is the respinse code #{response.code}"
-        case response.code
-        when 200
-          p "It worked !"
-          #puts response
-          #puts result
-          #puts response.headers
-          #response
-        when 423
-          put "something 423?"
-          exit
-          #raise SomeCustomExceptionIfYouWant
-        else
-          #puts response
-          puts "SOMETHING WENT WRONG, ABORTING."
-          exit
-          
-          #response.return!(request, result, &block)
-        end
-      }
-      
-      
+  rescue NoMethodError
+    puts "image did not have latitude longitude, not uploading"
+    
+  end
+  
+  
 
 	
 }
